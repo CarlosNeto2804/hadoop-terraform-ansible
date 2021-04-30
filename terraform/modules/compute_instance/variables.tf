0882ebe5a8ddfcc16d73de1f846e0972cf09d864
Carlos Alberto <carloshtcursos@gmail.com>
2021-04-30 00:02:37 -0400

    configure compute instance

---
+++ terraform/modules/compute_instance/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  type = string
+}
+
+variable "user_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "count_worker" {
+  type = number
+}
+
+variable "count_manager" {
+  type = number
+}
+
+variable "subnetwork" {
+  type = string
+}
+
+variable "key_ssh" {
+  type = string
+}
